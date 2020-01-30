4b47ccbe01cff2578da3b54ece03b2cf6710de15
Jens Gohl <jens.schaa@posteo.de>
2020-01-30 09:01:26 +0100

    log stderr of failed shell command

--- app/shell/shell.py
+++ app/shell/shell.py
@@ -6,15 +6,22 @@ logger = logging.getLogger('logsmith')
 
 
 def run(command):
+    proc = None
     try:
         proc = sp.run(shlex.split(command),
                       stdout=sp.PIPE,
-                      check=True,
+                      stderr=sp.PIPE,
+                      timeout=3,
                       universal_newlines=True)
+        proc.check_returncode()
         return proc.stdout.rstrip()
     except FileNotFoundError:
         logger.warning(f'command {command} not found')
         return None
     except sp.CalledProcessError:
         logger.warning(f'command {command} failed')
+        if proc:
+            logger.warning(proc.stderr)
+        else:
+            logger.warning('could not fetch output')
         return None
