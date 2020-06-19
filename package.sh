e35e7e37d3b59e803b3cb2b308972ce40a8e0a12
Jens Gohl <jens@copperlion.de>
2020-06-19 12:53:32 +0200

    update package.sh and add modes like zip and global

--- package.sh
+++ package.sh
@@ -1,19 +1,33 @@
 #!/usr/bin/env bash
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 usage() { echo "Usage: package.sh [-a]"
           echo "    packages logsmith into a executable"
           echo ""
           echo "Options:"
           echo "    -l  build for linux"
           echo "    -w  build for windows"
+          echo "    -g  use system python"
+          echo "    -z  zip after build"
           exit 1; }
-
-while getopts "lwh" opt; do
+global_mode=false
+zip_mode=false
+ignore_mode=false
+while getopts "lwgzih" opt; do
   case ${opt} in
     l)
       mode="linux"
       ;;
     w)
       mode="windows"
+      ;;
+    g)
+      global_mode=true
+      ;;
+    z)
+      zip_mode=true
+      ;;
+    i)
+      ignore_mode=true
       ;;
     h)
       usage
@@ -25,7 +39,20 @@ while getopts "lwh" opt; do
   esac
 done
 
-if [[ ! -d ./venv/ ]]; then
+local_changes=$(git status --porcelain -uno)
+untracked_files=$(git ls-files --others --exclude-standar)
+if ! ${ignore_mode} && [ "${local_changes}" != "" ]; then
+    echo -e " local changes found."
+    echo "${local_changes}"
+    exit 1
+fi
+if ! ${ignore_mode} && [ "${untracked_files}" != "" ]; then
+    echo -e " untracked files found."
+    echo "${untracked_files}"
+    exit 1
+fi
+
+if [[ ! -d ./venv/ ]] && [[ "${global_mode}" == "false" ]]; then
     ./setup.sh
 fi
 if [[ -d ./dist/ ]]; then
@@ -35,17 +62,24 @@ if [[ -d ./build/ ]]; then
     rm -rf build
 fi
 
-dist_path=./dist/$(uname)
-./venv/bin/pyinstaller \
+if [[ "${global_mode}" == "false" ]]; then
+  bundler=${dir}/venv/bin/pyinstaller
+else
+  bundler="python3.8 -m PyInstaller"
+fi
+
+dist=$(uname)
+dist_path=${dir}/dist/${dist}
+${bundler} \
     --onefile \
-    --distpath ${dist_path} \
+    --distpath "${dist_path}" \
     ./logsmith.spec
 
 if [[ "$(uname)" == "Darwin" ]] && [[ "${mode}" == "linux" ]]; then
     docker run \
         -v "$(pwd):/src/" \
         cdrx/pyinstaller-linux \
-        "pyinstaller --onefile --hidden-import PyQt5.sip --distpath ./dist/linux ./logsmith.spec"
+        "pyinstaller --onefile --hidden-import PyQt5.sip --distpath ${dist_path} ./logsmith.spec"
 fi
 
 if [[ "${mode}" == "windows" ]]; then
@@ -53,4 +87,10 @@ if [[ "${mode}" == "windows" ]]; then
         -v "$(pwd):/src/" \
         cdrx/pyinstaller-windows \
         "pyinstaller --onefile --hidden-import PyQt5.sip ./logsmith.spec"
+fi
+
+if ${zip_mode}; then
+  version=$(python -c "from app.__version__ import __version__; print('.'.join(str(i) for i in __version__))" )
+  cd "${dist_path}" || exit
+  zip -r logsmith_${dist}_${version}.zip ./logsmith*
 fi
