14a3a5fa736079ee842564499dca63af8512b505
Jens Gohl <jens@copperlion.de>
2023-04-21 14:06:50 +0200

    setup should only try to add a venv if its not present

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-pip3 install --user virtualenv
-python3 -m virtualenv -p python3.8 venv
+if [ ! -d ${dir}/venv ]; then
+  pip3 install --user virtualenv
+  python3 -m virtualenv -p python3 venv
+fi
 
 ./venv/bin/pip3 install -r requirements.txt
