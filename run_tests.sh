ff97d7ac990b241a0dafe507bc3d0bdf4bf18da4
Jens Gohl <jens@copperlion.de>
2024-01-03 14:19:51 +0100

    run_tests should try to execute tests without venv

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 cd tests || exit
-../venv/bin/nosetests
+
+if [ -d ../venv ]; then
+  echo "Virtual environment found. Using nose2 from virtual environment to run tests"
+  ../venv/bin/nosetests
+else
+  echo "Virtual environment not found. Trying system wide nose2 to run tests"
+  nosetests
+fi
