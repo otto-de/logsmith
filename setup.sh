b52ddf9d6926b947004c5e0bee252f0a97c78c41
Jens Gohl <jens@copperlion.de>
2025-01-15 15:19:27 +0100

    add missing variable to determine project dir

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-
+dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 if [ ! -d ${dir}/venv ]; then
   pip3 install --user virtualenv
   python3 -m virtualenv -p python3 venv
