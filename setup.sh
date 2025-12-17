687334fa629f618885cbfff986cc92c7f0695bae
Jens Gohl <jens@copperlion.de>
2025-12-17 14:54:53 +0100

    do not install virtualenv for the user anymore

--- setup.sh
+++ setup.sh
@@ -2,8 +2,7 @@
 set -e
 dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 if [ ! -d ${dir}/venv ]; then
-  pip3 install --user virtualenv
-  python3 -m virtualenv -p python3 venv
+  python3 -m venv .venv
 fi
 
-./venv/bin/pip3 install -r requirements.txt
+./.venv/bin/pip3 install -r requirements.txt
