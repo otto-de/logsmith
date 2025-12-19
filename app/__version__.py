03175adb1b6961714ab1d4ba6cad0dfb4e4be00c
Jens Gohl <jens@copperlion.de>
2025-12-19 13:05:20 +0100

    add option to include prerelease to __version_string__

--- app/__version__.py
+++ app/__version__.py
@@ -1,2 +1,6 @@
 __version__ = (9, 0, 2)
+__prerelease__ = None
+
 __version_string__ = '.'.join(str(i) for i in __version__)
+if __prerelease__:
+    __version_string__ = f'{__version_string__}-{__prerelease__}'
